843282394ac9dc1ae52d82f3c19710a68b6a4e42
Josh Nussbaum <joshnuss@gmail.com>
2019-11-27 20:05:27 -0500

    Adds example of local public state

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,24 @@
 <script>
   import LocalPrivateState from './examples/LocalPrivateState.svelte'
+  import LocalPublicState from './examples/LocalPublicState.svelte'
+
+  let boundValue = 0;
 </script>
 
 <main>
-  <LocalPrivateState/>
+  <section>
+    <h2>Local Private State</h2>
+    <LocalPrivateState/>
+  </section>
+
+  <section>
+    <h2>Local Public State</h2>
+    <LocalPublicState value={42}/>
+  </section>
+
+  <section>
+    <h2>Local Public State</h2>
+    <LocalPublicState bind:value={boundValue}/>
+    <p>It's {boundValue} in the parent.</p>
+  </section>
 </main>
---
+++ src/examples/LocalPublicState.svelte
@@ -0,0 +1,10 @@
+<script>
+  // exported, so it can be set or bound by parent
+  export let value = 0
+
+  function increment() {
+    value = value + 1
+  }
+</script>
+
+{value} <button on:click|preventDefault={increment}>++</button>
