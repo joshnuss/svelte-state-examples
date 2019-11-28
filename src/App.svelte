13856e31ae7f7b5937ed1f427d544f683cb4bc9e
Josh Nussbaum <joshnuss@gmail.com>
2019-11-28 01:38:18 -0500

    Clean up titles

--- src/App.svelte
+++ src/App.svelte
@@ -11,26 +11,26 @@
 <main>
   <h1>Svelte.js state management options</h1>
   <section>
-    <h2>Local Private State</h2>
+    <h2>Private State</h2>
     <p>Only the component has access.<br>Lost once the component is removed from the DOM.</p>
     <LocalPrivateState/>
   </section>
 
   <section>
-    <h2>Local Parent-Accessable State</h2>
+    <h2>Parent-Accessable State</h2>
     <p>The component and the component's owner have access.<br>Lost once the owner is removed from the DOM. <br>One-way binding.</p>
     <LocalPublicState value={42}/>
   </section>
 
   <section>
-    <h2>Local Bound State</h2>
+    <h2>Bound State</h2>
     <p>The component and the component's owner have access.<br>Lost once the owner is removed from the DOM. <br>Two-way binding.</p>
     <LocalPublicState bind:value={boundValue}/>
     <p>It's also {boundValue} in the parent.</p>
   </section>
 
   <section>
-    <h2>Local Tree State</h2>
+    <h2>Component Tree State</h2>
     <p>The component and all it's descendants have access.<br>Lost once the root component is removed from the DOM.</p>
     <LocalTreeState/>
   </section>
